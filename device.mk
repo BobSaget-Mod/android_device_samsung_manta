9ace57a5be29c3b1f8763f3afdd434d78a6ac108
Stephen Smalley <sds@tycho.nsa.gov>
2012-11-19 10:24:02 -0500

    Use our locally built kernel with SELinux enabled.

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/samsung/manta/kernel
+LOCAL_KERNEL := kernel/exynos/arch/arm/boot/zImage
 else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
